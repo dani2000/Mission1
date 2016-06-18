866083d1795cba79444dff230fd79e298f3d9cd5
Bluedrack28 <Bluedrack28@gmail.com>
2016-06-18 16:27:51 +0200

    Modif

--- src/Calage.java
+++ src/Calage.java
@@ -1,8 +1,10 @@
+import lejos.ev3.tools.EV3MapCommand;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.robotics.SampleProvider;
 
 public class Calage {
-	public void Calage(EV3LargeRegulatedMotor roueDroite, EV3LargeRegulatedMotor roueGauche,SampleProvider color){
+	public void Calage(EV3LargeRegulatedMotor roueDroite, EV3LargeRegulatedMotor roueGauche,EV3MediumRegulatedMotor moteusrFrappe, SampleProvider color){
 		
 	}
 }
